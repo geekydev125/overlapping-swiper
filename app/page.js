ae1b603cabe0a27a222f271c99657cffe0ad975f
geekydev125 <goldenstar1108@gmail.com>
2024-10-01 11:46:41 +0800

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,81 @@
-import Image from "next/image";
+'use client'
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCards, Navigation } from 'swiper/modules';
+// Icons
+import { CiCircleChevRight } from "react-icons/ci";
 
+
+import 'swiper/css';
+import 'swiper/css/effect-cards';
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [flgDirt, setFlgDirt] = useState("horizontal");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const remoteTranslate3D = () => {
+    for (let i = 0; i < 6; i++) {
+      let j = document.getElementsByClassName("swiper-slide")[i].style.transform;
+      document.getElementsByClassName("swiper-slide")[i].style.transform = j.replace(/translate3d\(([^,]+), ([^,]+), [^)]+\)/, 'translate3d($1, $2, 0px)');
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      remoteTranslate3D();
+      window.addEventListener('resize', handleResize);
+    }, [100])
+  }, [])
+
+  const handleResize = () => {
+    if (window) {
+      const newWidth = window.innerWidth;
+      newWidth <= 900 ? setFlgDirt("vertical") : setFlgDirt("horizontal");
+        setTimeout(() => {
+          remoteTranslate3D();
+        }, [100])
+    }
+  };
+
+
+  return (
+    <div className='mt-[100px]'>
+      <div className='relative w-min m-auto'>
+        <Swiper
+          effect={'cards'}
+          grabCursor={true}
+          modules={[EffectCards, Navigation]}
+          navigation={{
+            nextEl: ".swiper-next",
+            prevEl: ".swiper-prev",
+          }}
+          onSwiper={(swiper) => {
+            console.log("1");
+            remoteTranslate3D();
+          }
+          }
+          onSlideChange={() => {
+            remoteTranslate3D();
+          }}
+          spaceBetween={-485}
+          slidesPerView={3}
+          cardsEffect={{
+            rotate: false,
+            perSlideOffset: 15,
+          }}
+          allowSlidePrev={false}
+          loop={true}
+          className='h-[320px]'
+          direction={flgDirt}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <SwiperSlide className='slider-1 flex justify-center items-center'><div className='h-full'></div></SwiperSlide>
+          <SwiperSlide className='slider-2 flex justify-center items-center'><div className='h-full'></div></SwiperSlide>
+          <SwiperSlide className='slider-3 flex justify-center items-center'><div className='h-full'></div></SwiperSlide>
+          <SwiperSlide className='slider-4 flex justify-center items-center'><div className='h-full'></div></SwiperSlide>
+          <SwiperSlide className='slider-5 flex justify-center items-center'><div className='h-full'></div></SwiperSlide>
+          <SwiperSlide className='slider-6 flex justify-center items-center'><div className='h-full'></div></SwiperSlide>
+        </Swiper>
+        <button className='dir-btn absolute swiper-next z-[100] top-[47%] right-[10px]'><CiCircleChevRight className='fill-white text-[30px] font-bold' /></button>
+      </div>
+
     </div>
   );
 }
